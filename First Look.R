b75b189bd614549c4db0ef2d8515f9d8b519fc97
sebycart <sc4963@columbia.edu>
2022-04-23 15:54:57 -0400

    new data

--- First Look.R
+++ First Look.R
@@ -62,10 +62,16 @@ ye_blake_old = rbind(ye %>%
 
 ggparcoord(ye_blake_old, columns = 3:13, alphaLines = .6, splineFactor = 10, groupColumn = "album_name")
 
-a =get_tracks("15KC3dbhLApMz6YnuT3gjn", market = "US")
-
 kanye_mostpopular = get_artist_top_tracks(id = "5K4W6rqBFWDnAN6FQUkS6x",
                                           market = "US")
+
+kanye_mostpopularUK = get_artist_top_tracks(id = "5K4W6rqBFWDnAN6FQUkS6x",
+                                          market = "UK")
+
+
+
+
+
 
 ##Kanye Information 
 ye_info = get_artist(id = "5K4W6rqBFWDnAN6FQUkS6x")
@@ -76,6 +82,49 @@ ye_info[["popularity"]]
 
 ##Blake shelton
 get_artist(id = "1UTPBmNbXNTittyMJrNkvw")[["popularity"]]
+
+
+
+
+####
+beatles <- get_artist_audio_features('the beatles')
+
+defjam_artists = get_label_artists(label = "Def Jam")
+
+###Genres
+
+
+rap = get_genre_artists(genre = "rap")
+country = get_genre_artists(genre = "country")
+get_categories()
+
+similartokanye = get_related_artists(id = "5K4W6rqBFWDnAN6FQUkS6x")
+
+kanye = get_discography("kanye") 
+
+kanye_album = kanye %>% 
+  select(artist_name, artist_id,album_id, album_name,track_name, track_n,track_number, energy )
+
+kanye_energy = kanye %>% 
+  group_by(track_name) %>% 
+  summarise(energy2 = mean(energy))
+
+kanye_energy_spread = kanye_energy %>% 
+  pivot_wider(id_cols = c(album_id, album_name),
+              names_from = track_number,
+              values_from = energy)
+kanye_energy_spread[is.na(kanye_energy_spread)] <- 0
+
+ggparcoord(kanye_energy_spread, 
+           columns = 3:9, 
+           alphaLines = .3, 
+           splineFactor = 10, 
+           groupColumn = "album_name")
+
+
+
+
+
 
 
 
